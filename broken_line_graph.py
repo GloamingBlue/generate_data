d1ae3c35eddd038206a9391c9531254dc43009d0
GloamingBlue <1992491680@qq.com>
2022-02-28 15:02:50 +0800

    绘制简单的折线图plus

--- broken_line_graph.py
+++ broken_line_graph.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5, 6]
 squares = [1, 4, 9, 16, 25, 36]
+plt.style.use('seaborn')
 fg, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+# 设置图表标题并给坐标轴加上标签
+ax.set_title("Square", fontsize=24)
+ax.set_xlabel("value", fontsize=14)
+ax.set_ylabel("squares of value", fontsize=14)
+
+# 设置刻度标记的大小
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
